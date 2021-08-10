3df9f28099ce349e6ff3e38401b9242239d2abe6
Victor Hugo Cordeiro Correa <viktorhugo.dev@gmail.com>
2021-08-10 20:58:39 -0300

    Refatoração do modal com class

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
+/* eslint-disable import/no-named-as-default */
 
 import ScrollSuave from './modules/scroll-suave.js'
 import Accordion from './modules/accordion.js'
 import TabNav from './modules/tabnav.js'
-import initModal from './modules/modal.js'
+import Modal from './modules/modal.js'
 import initTooltip from './modules/tooltip.js'
 import initDropdownMenu from './modules/dropdown-menu.js'
 import initMenuMobile from './modules/menu-mobile.js'
@@ -20,8 +21,10 @@ accordion.init()
 const tabNav = new TabNav('[data-tab="menu"] li', '[data-tab="content"] section')
 tabNav.init()
 
+const modal = new Modal('[data-modal="abrir"]', '[data-modal="fechar"]', '[data-modal="container"]')
+modal.init()
+
 initAnimacaoScroll()
-initModal()
 initTooltip()
 initDropdownMenu()
 initMenuMobile()
