eea194fff9db931d2cadd2f7d5f88a3c3573106e
Victor Hugo Cordeiro Correa <viktorhugo.dev@gmail.com>
2021-08-10 12:51:56 -0300

    bugfix git

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 import ScrollSuave from './modules/scroll-suave.js'
-import initAnimacaoScroll from './modules/scroll-animacao.js'
 import initAccordion from './modules/accordion.js'
+import initAnimacaoScroll from './modules/scroll-animacao.js'
 import initTabNav from './modules/tabnav.js'
 import initModal from './modules/modal.js'
 import initTooltip from './modules/tooltip.js'
