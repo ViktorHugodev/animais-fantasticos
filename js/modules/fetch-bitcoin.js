7ae1b7e55b6661e4dc2dfb8ed765d7afd49c084a
Victor Hugo Cordeiro Correa <viktorhugo.dev@gmail.com>
2021-08-10 19:23:15 -0300

    Refatoração de funções

--- js/modules/fetch-bitcoin.js
+++ js/modules/fetch-bitcoin.js
@@ -2,7 +2,7 @@ export default function initFetchBitcoin() {
   fetch('https://blockchain.info/ticker')
     .then(response => response.json())
     .then((bitcoin) => {
-      const btcPreco = document.querySelector('.btc-preco');
-      btcPreco.innerText = (10 / bitcoin.BRL.sell).toFixed(6);
-    }).catch(erro => console.log(Error(erro)));
+      const btcPreco = document.querySelector('.btc-preco')
+      btcPreco.innerText = (10 / bitcoin.BRL.sell).toFixed(6)
+    }).catch(erro => console.log(Error(erro)))
 }
--- js/modules/tabnav.js
+++ js/modules/tabnav.js
@@ -1,21 +1,21 @@
 export default function initTabNav() {
-  const tabMenu = document.querySelectorAll('[data-tab="menu"] li');
-  const tabContent = document.querySelectorAll('[data-tab="content"] section');
+  const tabMenu = document.querySelectorAll('[data-tab="menu"] li')
+  const tabContent = document.querySelectorAll('[data-tab="content"] section')
 
   function activeTab(index) {
     tabContent.forEach((section) => {
-      section.classList.remove('ativo');
-    });
-    const direcao = tabContent[index].dataset.anime;
-    tabContent[index].classList.add('ativo', direcao);
+      section.classList.remove('ativo')
+    })
+    const direcao = tabContent[index].dataset.anime
+    tabContent[index].classList.add('ativo', direcao)
   }
 
   if (tabMenu.length && tabContent.length) {
-    tabContent[0].classList.add('ativo');
+    tabContent[0].classList.add('ativo')
     tabMenu.forEach((itemMenu, index) => {
       itemMenu.addEventListener('click', () => {
-        activeTab(index);
-      });
-    });
+        activeTab(index)
+      })
+    })
   }
 }
